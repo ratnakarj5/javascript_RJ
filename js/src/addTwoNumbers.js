6af6ac0695f346a85a5688c782a0cb0b5479822c
jr_projects <j.ratnakar5@gmail.com>
2018-07-01 16:57:46 +0530

    addition of two numbers

---
+++ js/src/addTwoNumbers.js
@@ -0,0 +1,10 @@
+Adds two numbers together
+
+Written 25 Jan 16 by Sam Student
+for CIS 201 Assignment 2
+help received from Sally Scriptwhiz
+--------------------------------- */
+var x = +prompt("Enter a number");
+var y = +prompt("Enter another number");
+var z = x + y;
+alert("The sum is " + z);
